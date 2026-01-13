a7f27a29c2d62a205be066f65bc785a93b5eb6a2
MoamenAbouhaty95 <momenabohaty@gmail.com>
2026-01-13 02:12:55 +0200

    Add basic examples for arithmetic, relational, logical, and assignment operators

---
+++ operators/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 3;
+
+    // Arithmetic Operators
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << endl;
+    cout << "a % b = " << a % b << endl;
+
+    // Relational Operators
+    cout << "a == b: " << (a == b) << endl;
+    cout << "a != b: " << (a != b) << endl;
+    cout << "a > b: " << (a > b) << endl;
+    cout << "a < b: " << (a < b) << endl;
+    cout << "a >= b: " << (a >= b) << endl;
+    cout << "a <= b: " << (a <= b) << endl;
+
+    // Logical Operators
+    bool x = true, y = false;
+    cout << "x && y: " << (x && y) << endl;
+    cout << "x || y: " << (x || y) << endl;
+    cout << "!x: " << (!x) << endl;
+
+    // Assignment Operators
+    int c = 5;
+    c += 2; // c = c + 2
+    cout << "c after += 2: " << c << endl;
+
+    c *= 3; // c = c * 3
+    cout << "c after *= 3: " << c << endl;
+
+    return 0;
+}
